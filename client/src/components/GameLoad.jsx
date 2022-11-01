b56e7da7fa5d300a931d4e65c56d013370f9e853
FIRST_NAME LAST_NAME <ibrahim.mowafy10@gmail.com>
2022-11-01 20:44:19 +0200

    Adding game load component

---
+++ client/src/components/GameLoad.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { usGlobalContext } from '../context';
+import CustomButton from './CustomButton';
+import { player01, player02 } from '../assets';
+import styles from '../styles';
+
+const GameLoad = () => {
+  const { walletAddress } = usGlobalContext();
+  const navigate = useNavigate();
+
+  return (
+    <div className={`${styles.flexBetween} ${styles.gameLoadContainer}`}>
+      <div className={styles.gameLoadBtnBox}>
+        <CustomButton
+          title="Choose Battleground"
+          handleClick={() => navigate('/battleground')}
+          restStyles="mt-6"
+        />
+      </div>
+      <div className={`flex-1 ${styles.flexCenter} flex-col`}>
+        <h1 className={`${styles.headText} text-center`}>
+          Waiting for a <br /> worthy opponent...
+        </h1>
+        <p className={styles.gameLoadText}>
+          Pro tip: While you're waiting, choose your preferred battleground
+        </p>
+
+        <div className={styles.gameLoadPlayersBox}>
+          <div className={`${styles.flexCenter} flex-col`}>
+            <img
+              src={player01}
+              className={styles.gameLoadPlayerImg}
+              alt="player 1"
+            />
+            <p className={styles.gameLoadPlayerText}>
+              {walletAddress.slice(0, 30)}
+            </p>
+          </div>
+          <h2 className={styles.gameLoadVS}>Vs</h2>
+          <div className={`${styles.flexCenter} flex-col`}>
+            <img
+              src={player02}
+              className={styles.gameLoadPlayerImg}
+              alt="player 2"
+            />
+            <p className={styles.gameLoadPlayerText}>
+              ???????????????????????????
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GameLoad;
