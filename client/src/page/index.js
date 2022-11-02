1c8c89fd2e1a7deb8e639066ed06efacd1697d7f
FIRST_NAME LAST_NAME <ibrahim.mowafy10@gmail.com>
2022-11-02 18:18:27 +0200

    Adding battle page

--- client/src/page/index.js
+++ client/src/page/index.js
@@ -1,4 +1,6 @@
 import Home from './Home';
 import CreateBattle from './CreateBattle';
 import JoinBattle from './JoinBattle';
-export { Home, CreateBattle, JoinBattle };
+import Battle from './Battle';
+import Battleground from './Battleground';
+export { Home, CreateBattle, JoinBattle, Battle, Battleground };
