e96bdf825c75b7e87a67e7b4603d691bc9cd6631
FIRST_NAME LAST_NAME <ibrahim.mowafy10@gmail.com>
2022-12-29 17:21:05 +0200

    Adding alert message

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,68 +1,78 @@
-import React, { useState } from 'react';
-import { useGlobalContext } from '../context';
-import { PageHOC, CustomInput, CustomButton } from '../components';
-import { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+
+import { CustomButton, CustomInput, PageHOC } from '../components';
+import { useGlobalContext } from '../context';
+
 const Home = () => {
-  const { contract, walletAddress, setShowAlert } = useGlobalContext();
+  const { contract, walletAddress, gameData, setShowAlert, setErrorMessage } = useGlobalContext();
   const [playerName, setPlayerName] = useState('');
   const navigate = useNavigate();
 
   const handleClick = async () => {
     try {
       const playerExists = await contract.isPlayer(walletAddress);
+
       if (!playerExists) {
-        await contract.registerPlayer(playerName, playerName);
+        await contract.registerPlayer(playerName, playerName, { gasLimit: 500000 });
+
         setShowAlert({
           status: true,
           type: 'info',
           message: `${playerName} is being summoned!`,
         });
+
+        setTimeout(() => navigate('/create-battle'), 8000);
       }
     } catch (error) {
-      console.log(error);
-      setShowAlert({
-        status: true,
-        type: 'failure',
-        message: 'Something went wrong!',
-      });
+      setErrorMessage(error);
     }
   };
 
   useEffect(() => {
-    const checkForPlayerToken = async () => {
+    const createPlayerToken = async () => {
       const playerExists = await contract.isPlayer(walletAddress);
       const playerTokenExists = await contract.isPlayerToken(walletAddress);
 
       if (playerExists && playerTokenExists) navigate('/create-battle');
     };
-    if (contract) checkForPlayerToken();
+
+    if (contract) createPlayerToken();
   }, [contract]);
 
+  useEffect(() => {
+    if (gameData.activeBattle) {
+      navigate(`/battle/${gameData.activeBattle.name}`);
+    }
+  }, [gameData]);
+
   return (
-    <div className="flex flex-col">
-      <CustomInput
-        label="Name"
-        placeholder="Enter your player name"
-        value={playerName}
-        handleValueChange={setPlayerName}
-      />
-      <CustomButton
-        title="Register"
-        handleClick={handleClick}
-        restStyles="mt-6"
-      />
-    </div>
+    walletAddress && (
+      <div className="flex flex-col">
+        <CustomInput
+          label="Name"
+          placeHolder="Enter your player name"
+          value={playerName}
+          handleValueChange={setPlayerName}
+        />
+
+        <CustomButton
+          title="Register"
+          handleClick={handleClick}
+          restStyles="mt-6"
+        />
+      </div>
+    )
   );
 };
 
 export default PageHOC(
   Home,
   <>
-    Welcome to Avax Gods <br />a Web3 NFT Game
+    Welcome to Avax Gods <br /> a Web3 NFT Card Game
   </>,
   <>
-    Connect your wallet to start playing <br />
-    the ultimate Web3 Battle Card Game
-  </>
+    Connect your wallet to start playing <br /> the ultimate Web3 Battle Card
+    Game
+  </>,
 );
