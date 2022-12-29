43f19d2c5761ff57b9f86313be20d72142c8f632
FIRST_NAME LAST_NAME <ibrahim.mowafy10@gmail.com>
2022-12-29 17:11:25 +0200

    Change contract

--- client/src/contract/index.js
+++ client/src/contract/index.js
@@ -1,5 +1,5 @@
 import contract from './AVAXGods.json';
 
-export const ADDRESS = '0x4627Dd323550a8527cF48A8F8CAf3E66De5975F3';
+export const ADDRESS = '0x51FD16f2e373Fa53Be1A8894A36486DB8fa79697';
 
 export const { abi: ABI } = contract;
