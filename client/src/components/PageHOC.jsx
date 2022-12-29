eea9e42a7c459e88336a52a5c7fefd8a07123edc
FIRST_NAME LAST_NAME <ibrahim.mowafy10@gmail.com>
2022-12-29 18:15:14 +0200

    Change in HOC

--- client/src/components/PageHOC.jsx
+++ client/src/components/PageHOC.jsx
@@ -27,7 +27,7 @@ const PageHOC = (Component, title, description) => () => {
           <Component />
         </div>
 
-        <p className={styles.footerText}>Made with 💜 by JavaScript Mastery</p>
+        <p className={styles.footerText}>Made with 💜 by Ibrahim Mowafy</p>
       </div>
 
       <div className="flex flex-1">
